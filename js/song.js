ab11110a44412890bb5180c5113197c7cc5c5e23
talkingtogod <lorisbaj2010@gmail.com>
2025-01-27 12:22:59 +0100

    Create song.js

---
+++ js/song.js
@@ -0,0 +1,7 @@
+const playlist = {
+    "song_1": "song.mp3",
+    "song_2": "audio/saw.mp3",
+    "song_3": "audio/marbles.mp3",
+    "song_4": "audio/seagulls.mp3",
+    "song_5": "audio/plane.mp3"
+};
